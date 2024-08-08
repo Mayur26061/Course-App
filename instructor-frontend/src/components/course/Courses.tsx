2a3961f1d0ae61856f4406da1571fc32ad679165
Mayur Rathod <mdridrdrr@gmail.com>
2024-08-09 00:31:18 +0530

    Bootstrap in TS

---
+++ instructor-frontend/src/components/course/Courses.tsx
@@ -0,0 +1,50 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+function Course(props) {
+  const navigate = useNavigate();
+  return (
+    <div tabIndex={0}>
+      <Card className="text-center m-2 w-72 h-80">
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.course.title}
+          </Typography>
+          <Typography
+            className="truncate h-5"
+            variant="body2"
+            color="text.secondary"
+          >
+            {props.course.description}
+          </Typography>
+          <div className="h-36 p-2">
+            <CardMedia
+              component="img"
+              alt="courseImage"
+              image={props.course.imageLink}
+              className="h-full w-full"
+            />
+          </div>
+          <div className="mt-5">
+            <Button
+              onClick={() => {
+                navigate(`/admin/course/${props.course._id}`);
+              }}
+              size="small"
+              variant="contained"
+            >
+              Edit
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+export default Course;
---
+++ instructor-frontend/src/components/course/ShowCourses.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Course from "./Courses";
+import { BASE_URL } from "../../config";
+function ShowCourses() {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/admin/courses`, {
+        headers: {
+          authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then((resposne) => {
+        setCourses(resposne.data.courses);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="flex justify-center flex-wrap">
+        {courses && courses.map((c) => <Course key={c._id} course={c} />)}
+      </div>
+    </div>
+  );
+}
+
+export default ShowCourses;
