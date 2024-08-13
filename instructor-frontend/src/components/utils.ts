1e800bf54448a77547fd67ab3e1b4d36f20e5878
Mayur Rathod <mdridrdrr@gmail.com>
2024-08-13 23:10:46 +0530

    Updated frontend routes for contents and ensure functional part

--- instructor-frontend/src/components/utils.ts
+++ instructor-frontend/src/components/utils.ts
@@ -33,7 +33,6 @@ export const boxStyle = {
   position: "absolute",
   top: "50%",
   left: "50%",
-  width: 500,
   transform: "translate(-50%, -50%)",
   bgcolor: "background.paper",
   boxShadow: 24,
