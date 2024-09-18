b0670099f78a13393007a0d468f5cf092a27d5b0
Mayur Rathod <mdridrdrr@gmail.com>
2024-09-19 00:29:26 +0530

    Building sidebar

--- admin-frontend/src/components/home/Sidebar.tsx
+++ admin-frontend/src/components/home/Sidebar.tsx
@@ -1,16 +1,52 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
+import { NavLink } from "react-router-dom";
 interface OptionalProps {
-  setShow?:React.Dispatch<React.SetStateAction<boolean>> ;
+  setShow?: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const Sidebar:FC<OptionalProps> = () => {
-
+const Sidebar: FC<OptionalProps> = () => {
+  const obj = [
+    {
+      route: "user",
+      title: "Users",
+    },
+    {
+      route: "course",
+      title: "Course",
+    },
+    {
+      route: "enroll",
+      title: "Enrolled Users",
+    },
+  ];
   return (
     <>
-    <div className="flex flex-col items-center min-w-350 h-full text-white">
-
-    </div>
+      <div className="flex flex-col items-center min-w-350 h-full">
+        {obj.map((data, id) => (
+          <SidebarItems route={data.route} title={data.title} key={id} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+interface items {
+  route: string;
+  title: string;
+}
+const SidebarItems: FC<items> = ({ route, title }) => {
+  return (
+    <div className="w-full flex justify-center">
+      <NavLink
+        to={"/" + route}
+        className={(({ isActive }) => isActive?'font-bold':'')}>
+        <h1
+          className="p-1 !text-inherit"
+        >
+          {title}
+        </h1>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Sidebar;
