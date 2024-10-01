fb092af6c5da24e321dcde29fe086d11c804dc43
Mayur Rathod <mdridrdrr@gmail.com>
2024-10-01 09:53:40 +0530

    Implemented course list

---
+++ admin-frontend/src/components/course/Course.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+import { TableCell,TableRow } from '@mui/material'
+import { courseType } from './CourseList'
+
+type courseprop = {
+    course:courseType
+}
+const Course:FC<courseprop> = ({course}) => {
+  return (
+    <TableRow
+    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+  >
+    <TableCell>
+      {course.title}
+    </TableCell>
+    <TableCell>{course.description}</TableCell>
+    <TableCell>{course.price}</TableCell>
+    <TableCell>
+      {course.published ? "published" : "unpublished"}
+    </TableCell>
+  </TableRow>
+  )
+}
+
+export default Course
