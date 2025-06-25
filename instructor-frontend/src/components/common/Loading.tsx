a08875bae2037649290a19f202dde18572990f06
Mayur Rathod <mdridrdrr@gmail.com>
2025-06-25 09:35:24 +0530

    Remove remaining instructor-frontend

--- instructor-frontend/src/components/common/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@mui/material";
-
-export const Loading = () => {
-  return (
-    <div className="flex flex-col justify-center items-center w-full h-screen">
-      <CircularProgress />
-    </div>
-  );
-};
