114e3b619c068e0e94722416be6310bddcd4cebe
Mayur Rathod <mdridrdrr@gmail.com>
2025-06-25 09:44:05 +0530

    Some Lints

--- admin-frontend/src/components/course/CourseList.tsx
+++ admin-frontend/src/components/course/CourseList.tsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,60 +7,56 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { fetchCourses } from "./fetch";
-import Course from './Course';
+import Course from "./Course";
 
 interface Userfield {
-  id:string;
-  name:string;
+  id: string;
+  name: string;
 }
 export interface courseType {
-    id: string;
-    createAt: Date;
-    updatedAt: Date;
-    title: string;
-    description: string;
-    price: string;
-    published: boolean;
-    image: string;
-    duration: Date | null;
-    author_id: string;
-    category: string | null;
-    author:Userfield;
-  }
-
-const CourseList = () => {
-    const [courses, setCourses] = useState<courseType[]>([]);
-    useEffect(() => {
-      async function getCourses() {
-        const course = await fetchCourses();
-        setCourses(course);
-      }
-      getCourses();
-    }, []);
-    return (
-      <TableContainer component={Paper} >
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell>Decription</TableCell>
-              <TableCell>Price</TableCell>
-              {/* <TableCell>Category</TableCell> */}
-              <TableCell>Status</TableCell>
-              <TableCell>Author</TableCell>
-              {/* <TableCell align="center">User Type</TableCell>
-              <TableCell align="center">Usename</TableCell>
-              <TableCell align="center">Status</TableCell> */}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {courses.map((row) => (
-                <Course key={row.id} course={row}/>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
+  id: string;
+  createAt: Date;
+  updatedAt: Date;
+  title: string;
+  description: string;
+  price: string;
+  published: boolean;
+  image: string;
+  duration: Date | null;
+  author_id: string;
+  category: string | null;
+  author: Userfield;
 }
 
-export default CourseList
+const CourseList = () => {
+  const [courses, setCourses] = useState<courseType[]>([]);
+  useEffect(() => {
+    async function getCourses() {
+      const course = await fetchCourses();
+      setCourses(course);
+    }
+    getCourses();
+  }, []);
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell>Decription</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Author</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {courses.map((row) => (
+            <Course key={row.id} course={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default CourseList;
